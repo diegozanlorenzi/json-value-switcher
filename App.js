1f76dc12e37bcc10427202e63db053030b7c09e8
Diego Zanlorenzi <diego_carreirozc@hotmail.com>
2018-12-13 00:25:05 +0100

    Update App.js with comments

--- App.js
+++ App.js
@@ -2,17 +2,16 @@ import JsonValueSwitcher from './lib/JsonValueSwitcher.js';
 
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('./file-settings.json').toString());
+//Change the desired input and output files in the file-settings.json
+const config  = JSON.parse(fs.readFileSync('./file-settings.json').toString());
 
 const json = JSON.parse(fs.readFileSync(`./input/${config.inputFileName}`).toString());
 
 let switchedJson = JsonValueSwitcher.switch(json);
 
 const data = new Uint8Array(Buffer.from(JSON.stringify(switchedJson, null, 2)));
+
 fs.writeFile(`./output/${config.outputFileName}`, data, (err) => {
   if (err) throw err;
-  console.log(`Switched JSON file ${config.outputFileName} generated`);
+  console.log(`Switched JSON file ${config.outputFileName} generated in folder /output`);
 });
-
-console.log(switchedJson);
-
